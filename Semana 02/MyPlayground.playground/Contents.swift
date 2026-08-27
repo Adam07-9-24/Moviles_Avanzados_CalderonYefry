29dbc84c31a2ed98fe545a2a90ec7bd2f46b40ea
Yefrycalderon <yefry.calderon@tecsup.edu.pe>
2026-08-27 12:33:34 -0500

    Calculo del promedio

--- Semana 02/MyPlayground.playground/Contents.swift
+++ Semana 02/MyPlayground.playground/Contents.swift
@@ -10,3 +10,13 @@ let alumno = readLine() ?? ""
 
 print("Nota del Examen Parcial:")
 let examenParcial = Double(readLine() ?? "") ?? 0
+print("Nota del Trabajo:")
+let notaTrabajo = Double(readLine() ?? "") ?? 0
+
+print("Nota del Examen Final:")
+let examenFinal = Double(readLine() ?? "") ?? 0
+
+// calculo del promedio
+let promedio = (examenParcial * parcial) +
+               (notaTrabajo * trabajo) +
+               (examenFinal * final)
