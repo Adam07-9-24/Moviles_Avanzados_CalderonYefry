9ebae607163a509fa2ed5dbdaae9adb803644788
Yefrycalderon <yefry.calderon@tecsup.edu.pe>
2026-08-27 13:17:32 -0500

    Calculo de multa

--- Semana 02/MyPlayground.playground/Contents.swift
+++ Semana 02/MyPlayground.playground/Contents.swift
@@ -12,3 +12,71 @@ let fechaPrestamo = readLine() ?? ""
 
 print("Fecha de Devolución (dd/MM/yyyy):")
 let fechaDevolucion = readLine() ?? ""
+
+let formatoFecha = DateFormatter()
+formatoFecha.dateFormat = "dd/MM/yyyy"
+
+let calendario = Calendar.current
+
+let prestamo = formatoFecha.date(from: fechaPrestamo)
+let devolucion = formatoFecha.date(from: fechaDevolucion)
+
+var diasOtorgados = 0
+var multaBase = 0.0
+
+switch tipoUsuario.lowercased() {
+case "alumno":
+    diasOtorgados = 7
+    multaBase = 1.50
+
+case "docente":
+    diasOtorgados = 15
+    multaBase = 2.00
+
+case "admin":
+    diasOtorgados = 10
+    multaBase = 3.00
+
+default:
+    diasOtorgados = 0
+    multaBase = 0.0
+}
+
+var fechaLimite: Date?
+var diasAtraso = 0
+var multaTotal = 0.0
+
+if let prestamo = prestamo, let devolucion = devolucion {
+
+    fechaLimite = calendario.date(
+        byAdding: .day,
+        value: diasOtorgados,
+        to: prestamo
+    )
+
+    if let limite = fechaLimite {
+        diasAtraso = max(
+            0,
+            calendario.dateComponents(
+                [.day],
+                from: limite,
+                to: devolucion
+            ).day ?? 0
+        )
+    }
+
+    if diasAtraso > 0 {
+        for dia in 1...diasAtraso {
+
+            if dia <= 3 {
+                multaTotal += multaBase
+
+            } else if dia <= 6 {
+                multaTotal += multaBase * 1.5
+
+            } else {
+                multaTotal += multaBase * 2
+            }
+        }
+    }
+}
