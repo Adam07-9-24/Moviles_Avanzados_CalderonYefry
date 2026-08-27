a40ed457e468308cec8bec00775dc148043eaae7
Yefrycalderon <yefry.calderon@tecsup.edu.pe>
2026-08-27 13:07:48 -0500

    Ingreso de datos del prestamo

--- Semana 02/MyPlayground.playground/Contents.swift
+++ Semana 02/MyPlayground.playground/Contents.swift
@@ -1,25 +1,14 @@
+
 import Foundation
 
-// definir el promedio por pesos
-let parcial = 0.3
-let trabajo = 0.3
-let final = 0.4
+print("Título del Libro:")
+let tituloLibro = readLine() ?? ""
 
-print("Nombre del Alumno")
-let alumno = readLine() ?? ""
+print("Tipo de Usuario (Alumno/Docente/Admin):")
+let tipoUsuario = readLine() ?? ""
 
-print("Nota del Examen Parcial:")
-let examenParcial = Double(readLine() ?? "") ?? 0
-print("Nota del Trabajo:")
-let notaTrabajo = Double(readLine() ?? "") ?? 0
+print("Fecha de Préstamo (dd/MM/yyyy):")
+let fechaPrestamo = readLine() ?? ""
 
-print("Nota del Examen Final:")
-let examenFinal = Double(readLine() ?? "") ?? 0
-
-// calculo del promedio
-let promedio = (examenParcial * parcial) +
-               (notaTrabajo * trabajo) +
-               (examenFinal * final)
-// mostrar los datos
-print("Alumno: \(alumno)")
-print("Promedio final: \(promedio)")
+print("Fecha de Devolución (dd/MM/yyyy):")
+let fechaDevolucion = readLine() ?? ""
