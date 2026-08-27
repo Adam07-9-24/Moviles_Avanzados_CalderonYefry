47e66a328c40f9191d8d0e84a9daf3f8469523ac
Yefrycalderon <yefry.calderon@tecsup.edu.pe>
2026-08-27 12:22:49 -0500

    ingreso de datos

--- Semana 02/MyPlayground.playground/Contents.swift
+++ Semana 02/MyPlayground.playground/Contents.swift
@@ -1,3 +1,12 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+// definir el promedio por pesos
+let parcial = 0.3
+let trabajo = 0.3
+let final = 0.4
+
+print("Nombre del Alumno")
+let alumno = readLine() ?? ""
+
+print("Nota del Examen Parcial:")
+let examenParcial = Double(readLine() ?? "") ?? 0
